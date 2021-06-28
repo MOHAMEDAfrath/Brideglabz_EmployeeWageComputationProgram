41bf31af2956cd90e70955a3f068b418d58feab1
MOHAMEDAfrath <afrathismath@gmail.com>
2021-06-28 10:41:28 +0530

    Employee Attendance UC is completed

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,9 +4,21 @@ namespace EmployeeWageComputation
 {
     class Program
     {
+        public const int FULL_TIME = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
+            Random random = new Random();
+            int employeeAttendance = random.Next(0, 2);
+            if(employeeAttendance == FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
         }
     }
 }
