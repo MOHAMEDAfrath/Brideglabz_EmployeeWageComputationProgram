2a00026cac38e107e60b01fcd25a92418a75f56f
MOHAMEDAfrath <afrathismath@gmail.com>
2021-06-28 12:00:05 +0530

    Calculate wage till total month or hour UC is completed

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,41 +8,41 @@ namespace EmployeeWageComputation
         public const int PART_TIME = 2;
         public const int TOTAL_WORKING_DAY = 20;
         public const int EMPLOYEE_WAGE_PER_HOUR = 20;
+        public const int EMPLOYEE_TOTAL_WORKING_HOUR = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
             int empHours = 0;
-            int empAbsentDays = 0;
-            int empFullTimeDays = 0;
-            int empPartTimeDays = 0;
+            int empTotalWorkingday = 1;
+            int empTotalWorkingHour = 0;
             int empPerDayWage = 0;
             int empTotalWage = 0; 
             Random random = new Random();
-            for (int workingday = 1; workingday <= TOTAL_WORKING_DAY; workingday++)
-            {
+            while(empTotalWorkingday <= TOTAL_WORKING_DAY && empTotalWorkingHour <= EMPLOYEE_TOTAL_WORKING_HOUR )
+            { 
                 int employeeAttendance = random.Next(0, 3);
                 switch (employeeAttendance)
                 {
                     case FULL_TIME:
                         empHours = 8;
-                        empFullTimeDays += 1;
                         break;
                     case PART_TIME:
                         empHours = 4;
-                        empPartTimeDays += 1;
                         break;
                     default:
-                        empAbsentDays += 1;
                         empHours = 0;
                         break;
                 }
                 empPerDayWage = empHours * EMPLOYEE_WAGE_PER_HOUR;
                 empTotalWage += empPerDayWage;
+                if (employeeAttendance > 0)
+                {
+                    empTotalWorkingday += 1;
+                }
+                empTotalWorkingHour += empHours;
             }
-            Console.WriteLine("Employee was absent for: "+empAbsentDays+ " days.");
-            Console.WriteLine("Employee was present full time for: " +empFullTimeDays+" days.");
-            Console.WriteLine("Employee was present Part time for: " + empPartTimeDays + " days.");
-            Console.WriteLine("Employee's wage for " + (TOTAL_WORKING_DAY - empAbsentDays) + "/20 days is "+empTotalWage);
+            Console.WriteLine("Employee's wage for Working Hour : " +empTotalWorkingday+ " and Working Hour : "+empTotalWorkingHour+ " is "+empTotalWage);
+            
             
 
         }
